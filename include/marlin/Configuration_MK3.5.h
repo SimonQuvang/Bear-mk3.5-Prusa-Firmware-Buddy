c90fcb904d3c06bd1553f6c9e0eae1f9d6c44846
SimonQuvang <simon.quvang@gmail.com>
2025-03-31 00:58:39 +0200

    back to 55 due to mintemp error

--- include/marlin/Configuration_MK3.5.h
+++ include/marlin/Configuration_MK3.5.h
@@ -805,9 +805,9 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT_E0 3152 // 95% of the original 280 to account for normalised flow
+#define DEFAULT_AXIS_STEPS_PER_UNIT_E0 788 // 95% of the original 280 to account for normalised flow
 #define DEFAULT_AXIS_STEPS_PER_UNIT \
-    { 1600, 1600, 3200, DEFAULT_AXIS_STEPS_PER_UNIT_E0 }
+    { 200, 200, 400, DEFAULT_AXIS_STEPS_PER_UNIT_E0 }
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
--- include/marlin/Configuration_MK3.5_adv.h
+++ include/marlin/Configuration_MK3.5_adv.h
@@ -1558,7 +1558,7 @@
 #if HAS_TRINAMIC
 
     constexpr float HOLD_MULTIPLIER[4] = {1, 1, 1, 1};  // Scales down the holding current from run current
-    #define INTERPOLATE false // Interpolate X/Y/Z_MICROSTEPS to 256
+    #define INTERPOLATE true // Interpolate X/Y/Z_MICROSTEPS to 256
 
     #if X_DRIVER_TYPE == TMC2130 && Y_DRIVER_TYPE == X_DRIVER_TYPE
         #define HAS_TMC_WAVETABLE // enable wavetable correction for this driver/motor type
@@ -1566,11 +1566,11 @@
 
     #if AXIS_IS_TMC(X)
         #define X_CURRENT 1200 // (mA) RMS current.  Custom LDO 400 step motors
-        #define X_MICROSTEPS 128 // 0..256
+        #define X_MICROSTEPS 16 // 0..256
         #define X_400_STEP_CURRENT 1200
-        #define X_400_STEP_MICROSTEPS 128 // 0..256
+        #define X_400_STEP_MICROSTEPS 16 // 0..256
         #define X_200_STEP_CURRENT 1200
-        #define X_200_STEP_MICROSTEPS 128 // 0..256
+        #define X_200_STEP_MICROSTEPS 16 // 0..256
         #define X_RSENSE 0.22
         #define X_CHAIN_POS 0
     #endif
@@ -1584,12 +1584,12 @@
     #if AXIS_IS_TMC(Y)
         // Use 400_STEP values as default current and microstep. For 200 step motors (MK3.9) the values will
         // be reconfigured in runtime if the printer is configured as MK3.9 in EEPROM.
-        #define Y_CURRENT 1000 // (mA) RMS current.   Custom LDO 400 step motors
-        #define Y_MICROSTEPS 128
+        #define Y_CURRENT 1200 // (mA) RMS current.   Custom LDO 400 step motors
+        #define Y_MICROSTEPS 16
         #define Y_400_STEP_CURRENT 1200
-        #define Y_400_STEP_MICROSTEPS 128 // 0..256
+        #define Y_400_STEP_MICROSTEPS 16 // 0..256
         #define Y_200_STEP_CURRENT 1200
-        #define Y_200_STEP_MICROSTEPS 128 // 0..256
+        #define Y_200_STEP_MICROSTEPS 16 // 0..256
         #define Y_RSENSE 0.22
         #define Y_CHAIN_POS 0
     #endif
@@ -1601,8 +1601,8 @@
     #endif
 
     #if AXIS_IS_TMC(Z)
-        #define Z_CURRENT 600
-        #define Z_MICROSTEPS 128
+        #define Z_CURRENT 800
+        #define Z_MICROSTEPS 64
         #define Z_RSENSE 0.22
         #define Z_CHAIN_POS 0
     #endif
@@ -1620,8 +1620,8 @@
     #endif
 
     #if AXIS_IS_TMC(E0)
-        #define E0_CURRENT 500
-        #define E0_MICROSTEPS 128
+        #define E0_CURRENT 450
+        #define E0_MICROSTEPS 32
         #define E0_RSENSE 0.22
         #define E0_CHAIN_POS 0
     #endif
